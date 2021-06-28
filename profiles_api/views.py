7e393d5e8a5985a72c9bc94426dbae84cdbe92e6
MrSor9ry <sor9rycoding@gmail.com>
2021-06-28 18:58:08 +0800

    ''

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializer
+
+
 
 class testApiView(APIView):
+
+    serializer_class = serializer.TestSerializer
 
     def get(self, request , format=None):
 
@@ -19,3 +25,22 @@ class testApiView(APIView):
         ]
 
         return Response({'message':'Testing','an_apiview':an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status =status.HTTP_400_BAD_REQUEST )
+
+    def put(self, request, pk=None):
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'Method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'Method':'DELETE'})
