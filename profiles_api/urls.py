b113eabdc36dcb8d170916316f8b9a154ffb079b
MrSor9ry <sor9rycoding@gmail.com>
2021-06-29 18:49:46 +0800

    ''

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,8 +5,8 @@ from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 router = DefaultRouter()
-
 router.register('test-viewset', views.testViewSet, basename='test-viewset' )
+router.register('profile', views.UserProfileViewSet)
 
 app_name='api'
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,18 +2,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializer
-
-
+from profiles_api import models
+from profiles_api import permissions
 
 class testApiView(APIView):
-
     serializer_class = serializer.TestSerializer
 
-    def get(self, request , format=None):
+    def get(self, request, format=None):
 
-        an_apiview =[
+        an_apiview = [
             # GET
             'api info content'
             # POST
@@ -26,7 +26,7 @@ class testApiView(APIView):
 
         ]
 
-        return Response({'message':'Testing','an_apiview':an_apiview})
+        return Response({'message': 'Testing', 'an_apiview': an_apiview})
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -34,31 +34,31 @@ class testApiView(APIView):
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Hello {name}'
-            return Response({'message':message})
+            return Response({'message': message})
         else:
-            return Response(serializer.errors, status =status.HTTP_400_BAD_REQUEST )
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk=None):
-        return Response({'method':'PUT'})
+        return Response({'method': 'PUT'})
 
     def patch(self, request, pk=None):
-        return Response({'Method':'PATCH'})
+        return Response({'Method': 'PATCH'})
 
     def delete(self, request, pk=None):
-        return Response({'Method':'DELETE'})
+        return Response({'Method': 'DELETE'})
+
 
 class testViewSet(viewsets.ViewSet):
-
     serializer_class = serializer.TestSerializer
 
     def list(self, request):
-        a_viewset =[
+        a_viewset = [
             'Uses action (list, create, retrieve, update, partial_update)',
             'Automatically maps to URLs using Routers',
             'Provides more functionally with less code',
         ]
 
-        return Response({'message':'Testing', 'a_viewset':a_viewset})
+        return Response({'message': 'Testing', 'a_viewset': a_viewset})
 
     def create(self, request):
 
@@ -67,25 +67,32 @@ class testViewSet(viewsets.ViewSet):
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Hello {name} !'
-            return  Response({'message':message})
+            return Response({'message': message})
         else:
             return Response(
                 serializer.errors,
-                status= status.HTTP_400_BAD_REQUEST
+                status=status.HTTP_400_BAD_REQUEST
             )
 
     def retrieve(self, request, pk=None):
 
-        return Response({'http_method':'GET'})
+        return Response({'http_method': 'GET'})
 
     def update(self, request, pk=None):
 
-        return Response({'http_method':'PUT'})
+        return Response({'http_method': 'PUT'})
 
     def partial_update(self, request, pk=None):
 
-        return Response({'http_method':'PATCH'})
+        return Response({'http_method': 'PATCH'})
 
     def destory(self, request, pk=None):
 
-        return Response({'http_method':'DELETE'})
+        return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializer.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
